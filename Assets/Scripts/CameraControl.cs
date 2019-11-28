b7ce2e718fe75f61bd3a770d62cdb55cab6fa1cb
Kraktu <Duponthphilip15@gmail.com>
2019-11-28 13:38:38 +0100

    Camera Repared

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -28,13 +28,10 @@ public class CameraControl : MonoBehaviour
 	{
 		while (true)
 		{
-			if (Input.GetKeyDown(KeyCode.LeftShift))
+
+			if (!_cameraDisabled)
 			{
-				_cameraDisabled = !_cameraDisabled;
-			}
-			if (Input.GetKey(KeyCode.Mouse1))
-			{
-				if (!_cameraDisabled)
+				if (Input.GetKey(KeyCode.Mouse1))
 				{
 					if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
 					{
@@ -42,24 +39,25 @@ public class CameraControl : MonoBehaviour
 						_localRotation.y -= Input.GetAxis("Mouse Y") * _mouseSensitivity;
 						_localRotation.y = Mathf.Clamp(_localRotation.y, 0, 90);
 					}
-
-					if (Input.GetAxis("Mouse ScrollWheel") != 0)
-					{
-						float ScrollAmount = Input.GetAxis("Mouse ScrollWheel") * _scrollSensitivity;
-						ScrollAmount *= (this._cameraDistance * 0.3f);
-						this._cameraDistance += ScrollAmount * -1;
-						this._cameraDistance = Mathf.Clamp(_cameraDistance, _minCameraDistance, _maxCameraDistance);
-					}
 				}
 
-				Quaternion QT = Quaternion.Euler(_localRotation.y, _localRotation.x, 0);
-				this._rotationCenter.rotation = Quaternion.Lerp(this._rotationCenter.rotation, QT, Time.deltaTime * _orbitDampening);
-
-				if (this._camera.localPosition.z != this._cameraDistance * -1)
+				if (Input.GetAxis("Mouse ScrollWheel") != 0)
 				{
-					this._camera.localPosition = new Vector3(0, 0, Mathf.Lerp(this._camera.localPosition.z, this._cameraDistance * -1, Time.deltaTime * _scrollDampening));
+					float ScrollAmount = Input.GetAxis("Mouse ScrollWheel") * _scrollSensitivity;
+					ScrollAmount *= (this._cameraDistance * 0.3f);
+					this._cameraDistance += ScrollAmount * -1;
+					this._cameraDistance = Mathf.Clamp(_cameraDistance, _minCameraDistance, _maxCameraDistance);
 				}
 			}
+
+			Quaternion QT = Quaternion.Euler(_localRotation.y, _localRotation.x, 0);
+			this._rotationCenter.rotation = Quaternion.Lerp(this._rotationCenter.rotation, QT, Time.deltaTime * _orbitDampening);
+
+			if (this._camera.localPosition.z != this._cameraDistance * -1)
+			{
+				this._camera.localPosition = new Vector3(0, 0, Mathf.Lerp(this._camera.localPosition.z, this._cameraDistance * -1, Time.deltaTime * _scrollDampening));
+			}
+
 			yield return null;
 		}
 	}
